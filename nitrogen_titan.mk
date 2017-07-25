9e27249f8bb038dd560c3f3aeb4c6edcdadd92a2
kranone124 <kranone1234@gmail.com>
2017-07-25 03:02:36 +0200

    Update nitrogen_titan.mk

--- nitrogen_titan.mk
+++ nitrogen_titan.mk
@@ -1,5 +1,10 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+
 # Inherit some common Nitrogen stuff.
 $(call inherit-product, vendor/nitrogen/products/common.mk)
 
